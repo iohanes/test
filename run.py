947f0aeb0fd83710e34b9e94ea24a4aefd1cc46c
Sasha <petrov.knu@gmail.com>
2016-04-18 14:17:52 +0200

    Calculated C_i from random walks data

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 
 import numpy as np
-from ROOT import TH1D
+from ROOT import TH1D, TFile, gROOT, TCanvas
 from Utilities import draw_and_save
 import matplotlib.pyplot as plt
 
@@ -12,7 +12,8 @@ class BinMoment(object):
 
     def get_bins(self):
         N = self.histogram.GetNbinsX()
-        bins = np.array([self.histogram.GetBinContent(i) for i in range(1, N + 1)])
+        print 'Warning manually deleting zero bins'
+        bins = np.array([self.histogram.GetBinContent(i) for i in range(1, N + 1) if self.histogram.GetBinContent(i)])
         return bins, N
 
     def get_moments(self):
@@ -30,6 +31,7 @@ class BinMoment(object):
 
 class TH1R(TH1D):
     def __init__(self, *arg):
+        gROOT.cd()
         super(TH1R, self).__init__(*arg)
 
     @staticmethod
@@ -57,33 +59,51 @@ class RandomWalker:
         result = map(lambda x, eps : self.random_step(lamda, x, eps), powers, epsilon)
         return np.sum(result)
 
-def get_histogram(name, title, lamda = 0.74):
+def gen_histogram(name, title, lamda = 0.74):
     walker = RandomWalker(30)
-    data  = walker.random_walk(10000, lamda)
+    data  = walker.random_walk(int(1e5), lamda)
     data, edges = np.histogram(data, 100)
 
     hist = TH1R.FromNpArray(data, edges, name, title)
     return hist
 
+def read_histos():
+    def process_hist(fname, title, Nbins = 10000):
+        data = TFile.Open(fname + '.root')
+        fname = fname + '_' + str(Nbins) 
+        a, b = data.random_walks.GetMinimum('gratio'), data.random_walks.GetMaximum('gratio')
+        hist = TH1R(fname, 'nbins = %d, ' % Nbins + title, Nbins, a, b)
+        data.random_walks.Draw('gratio >> ' + fname) 
+        return hist
+
+    a = process_hist('random_walks_data', 'Golden Ratio; x; counts')
+    b = process_hist('random_walks_data_nongold', '#lambda = 0.74; x; counts')
+    return a, b
+
+    # draw_and_save('test', True, True)
+
 
 def main():
-    hists = [get_histogram('normal', '#lambda = 0.74; x; counts'), get_histogram('golden', 'Golden Ratio; x; counts', ( (5 ** 0.5) - 1 ) / 2.)]
+    c1 = TCanvas('c1', 'Test', 800, 800)
+    # hists = [gen_histogram('normal', '#lambda = 0.74; x; counts'), gen_histogram('golden', 'Golden Ratio; x; counts', ( (5 ** 0.5) - 1 ) / 2.)]
+    hists = read_histos()
     colors = [38, 46]
 
     for h, c in zip(hists, colors):
         binner = BinMoment(h)
         coef = binner.get_moments()
         plt.plot(np.arange(coef.size), coef, label = h.GetTitle())
-        plt.legend()
+        plt.legend(loc='down center')
         plt.xlabel('j -- bin number')
         plt.ylabel('C_{j}')
         h.SetLineColor(c)
         h.Draw('hist same')
 
-    plt.savefig('res.png')
+    name = hists[0].GetName()
+    draw_and_save('hist_' + name, True, True)
+    plt.savefig('c_' + name + '.png')
     plt.show()
 
-    draw_and_save('test', True, True)
 
 
 if __name__ == '__main__':
